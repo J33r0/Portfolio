9a78d58e39752928cc9024fc43d38b6c8d6c5085
Jeroinimo Herdoiza <jeronimo.herdoiza@etu.unistra.fr>
2024-03-09 12:12:55 +0100

    add a footer template

---
+++ assets/templates/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <section>
+        <h6>Social Media</h6>
+            <ul>
+            <a href="">
+                <li>GitHub</li>
+            </a>
+            <a href="">
+                <li>Email</li>
+            </a>
+        </ul>
+    </section>
+</footer>
